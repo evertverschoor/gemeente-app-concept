6eb0fb4bfda8812de1eb1abf09f721e85876a2f9
Evert Verschoor <evert.verschoor@live.nl>
2020-02-22 13:26:33 +0100

    Change the UI

--- src/components/managers/window-manager/window-manager.jsx
+++ src/components/managers/window-manager/window-manager.jsx
@@ -6,9 +6,11 @@ import IncidentsWindow from '../../windows/incidents-window/incidents-window'
 class WindowManager extends React.Component {
 
     render() {
-        return <>
-            { this.shouldShow(IncidentsWindow) && <IncidentsWindow title="Incidenten" /> }
-        </>
+        return (
+            <>
+                {this.shouldShow(IncidentsWindow) && <IncidentsWindow title="Incidenten" />}
+            </>
+        )
     }
 
     shouldShow(window) {
